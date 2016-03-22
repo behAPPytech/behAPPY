739bfbf5e282224d749f8ee870907dae09b15536
Mia <mia@fryling.com>
2016-03-22 16:26:06 -0400

    schedule

---
+++ behAPPy/Assignments.swift
@@ -0,0 +1,23 @@
+//
+//  Assignments.swift
+//  behAPPy
+//
+//  Created by block7 on 3/22/16.
+//  Copyright © 2016 block7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Assignments {
+    
+    let title: String
+    
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    
+    
+}
